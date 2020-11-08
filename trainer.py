c0607e6af9a86ac29a02f7e014a53a1bea8fcf3f
Kate <arkhyarkhy@gmail.com>
2020-11-08 20:00:40 +0100

    tensorboard

--- trainer.py
+++ trainer.py
@@ -484,12 +484,12 @@ class AdvTrainer(BaseTrainer):
                     tp += ((onehot_pred.float() == 1) & (onehot_labels.float() == 1)).sum(dim=0).float()
                     fp += ((onehot_pred.float() == 1) & (onehot_labels.float() == 0)).sum(dim=0).float()
                     fn += ((onehot_pred.float() == 0) & (onehot_labels.float() == 1)).sum(dim=0).float()
+                    writer.add_scalar("Accuracy", correct_total / data_len, i)
+                    writer.add_scalars("Accuracy_by_class", summary_map(self.num_to_name, correct / data_len), i)
+                    writer.add_scalars("True_positives", summary_map(self.num_to_name, tp / data_len), i)
+                    writer.add_scalars("False_negatives", summary_map(self.num_to_name, fn / data_len), i)
+                    writer.add_scalars("False_positives", summary_map(self.num_to_name, fp / data_len), i)
                     if i % 1000 == 0:
-                        writer.add_scalars("Accuracy", correct_total / data_len, i)
-                        writer.add_scalars("Accuracy_by_class", summary_map(self.num_to_name, correct / data_len), i)
-                        writer.add_scalars("True_positives", summary_map(self.num_to_name, tp / data_len), i)
-                        writer.add_scalars("False_negatives", summary_map(self.num_to_name, fn / data_len), i)
-                        writer.add_scalars("False_positives", summary_map(self.num_to_name, fp / data_len), i)
                         print(
                             "Accuracy total {}, by class {}, tp {}, fp {}, fn {}".format(correct_total / data_len,
                                                                                          correct / data_len,
@@ -561,12 +561,12 @@ class AdvTrainer(BaseTrainer):
                 fn += ((onehot_pred.float() == 0) & (onehot_labels.float() == 1)).sum(dim=0).float()
 
                 msg = "{}/{} {} - ETA : {}" .format(i, num_batches, progress_bar(i, num_batches), eta(start, i, num_batches))
+                writer.add_scalar("Accuracy", correct_total / data_len, i)
+                writer.add_scalars("Accuracy_by_class", summary_map(self.num_to_name, correct / data_len), i)
+                writer.add_scalars("True_positives", summary_map(self.num_to_name, tp / data_len), i)
+                writer.add_scalars("False_negatives", summary_map(self.num_to_name, fn / data_len), i)
+                writer.add_scalars("False_positives", summary_map(self.num_to_name, fp / data_len), i)
                 if i % 1000 == 0:
-                    writer.add_scalars("Accuracy", correct_total / data_len, i)
-                    writer.add_scalars("Accuracy_by_class", summary_map(self.num_to_name, correct / data_len), i)
-                    writer.add_scalars("True_positives", summary_map(self.num_to_name, tp / data_len), i)
-                    writer.add_scalars("False_negatives", summary_map(self.num_to_name, fn / data_len), i)
-                    writer.add_scalars("False_positives", summary_map(self.num_to_name, fp / data_len), i)
                     print(
                         "Accuracy total {}, by class {}, tp {}, fp {}, fn {}".format(correct_total / data_len,
                                                                                      correct / data_len,
